a1cdbb1d33a1d01974e767199adc8bda20e4b5ea
LukaEP <lukae.paiva@gmail.com>
2020-08-03 16:50:34 -0300

    Handling email error

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,20 +1,24 @@
 const nodemailer = require('nodemailer');
 
 exports.sendEmail = async (req, res) => {
-    const transporter = nodemailer.createTransport({
-        service: process.env.USER_SERVICE_CONNECT,
-        auth: {
-            user: process.env.USER_EMAIL_CONNECT,
-            pass: process.env.USER_PASSWORD_CONNECT
-        }
-    });
+    try {
+        const transporter = nodemailer.createTransport({
+            service: process.env.USER_SERVICE_CONNECT,
+            auth: {
+                user: process.env.USER_EMAIL_CONNECT,
+                pass: process.env.USER_PASSWORD_CONNECT
+            }
+        });
 
-    let info = await transporter.sendMail({
-        from: req.body.from,
-        to: req.body.to,
-        subject: req.body.subject,
-        text: req.body.text
-    });
-    
-    res.status(200).send({message: "Email Sended"});
+        let info = await transporter.sendMail({
+            from: req.body.from,
+            to: req.body.to,
+            subject: req.body.subject,
+            text: req.body.text
+        });
+        
+        res.status(200).send({message: "Email Sended"});
+    } catch (error) {
+        res.status(500).send({message: `Error: ${error}`});
+    }
 };
